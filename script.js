9523833ad937e2992d2ed11fcb141a22cacbff25
gabrielfkm <gabrielhfkm@gmail.com>
2025-07-01 14:44:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const display = document.getElementById('display');
 
+function adicionar(valor) {
+  display.value += valor;
+}
+
+function limpar() {
+  display.value = '';
+}
+
+function calcular() {
+  try {
+    display.value = eval(display.value);
+  } catch (e) {
+    display.value = 'Erro';
+  }
+}
